574699d91d0de4598b6ff56564491b7c15b498a4
dank-meme-army <motherfan7@gmail.com>
2025-05-28 01:18:49 -0400

    new

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args){
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("type something here");
+    String Variable = in.nextLine();
+
+    //output stored input class
+
+    System.out.println (Variable);
+
+
+    }
+}
