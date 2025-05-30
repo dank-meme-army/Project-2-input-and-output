680f8635ac8169bd8248fbf31e875586d96e50d4
dank-meme-army <motherfan7@gmail.com>
2025-05-30 15:53:40 -0400

    w

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,31 @@ import java.util.Scanner;
 public class Main {
 
 public static void main(String[] args){
+Scanner in = new Scanner(System.in);
 
-    Scanner in = new Scanner(System.in);
+    //input seconds worked <- convert that into h/m/s h and s is realtive to s
+    //get the pay for the day. based on the rate
+    System.out.print("how long have you worked in seconds: ");
+    int totalSec = in.nextInt();
+    System.out.print("what is your hourly pay?: ");
+    double salary = in.nextInt();
 
-    System.out.println("type something here");
-    String Variable = in.nextLine();
+    int hours = totalSec/60;
+    int leftoverSec = totalSec % 3600;
+    int minutes = leftoverSec/60;
+    int secRemain = leftoverSec %60;
 
-    //output stored input class
 
-    System.out.println (Variable);
+    //display section
+
+    System.out.print("\n---Information Section----\n");
+    System.out.println("total time worked is: "+ hours+ " hours  "+ minutes+" minutes  " + secRemain + " seconds \n" );
+    System.out.printf("your total pay is $%.2f\n", salary);
+
+    in.close();
+
 
     }
 }
+
+
