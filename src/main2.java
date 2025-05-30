cf2bb7c021d4ecaa46bdfc846f2a4a008cc2e7d7
dank-meme-army <motherfan7@gmail.com>
2025-05-30 16:29:54 -0400

    change

---
+++ src/main2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner
+public class main2{
+    public static void main(String[] args){
+        //next project Grade Point Calculator
+        //it should look like 4 courses. ask for the grade. then show the letter grade for each.
+        //get teh average
+        //show if your on good academic standing or your failing with a message
+
+        Scanner in = new Scanner(System.in);
+
+        //section 1 input course name and grades for each
+        System.out.print("what is your first course: ");
+        String name1 = in.nextLine();
+        int course1 = in.nextInt();
+
+        System.out.print("2nd course name: ");
+        String name1 = in.nextLine();
+        int course2 = in.nextInt();
+
+        System.out.print("3rd course name: ");
+        String name1 = in.nextLine();
+        int course3 = in.nextInt();
+
+        System.out.print("4th course name: ");
+        String name1 = in.nextLine();
+
+
+
+    }
+}
